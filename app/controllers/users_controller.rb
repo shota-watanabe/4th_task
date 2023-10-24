83ab758b4cf07175bd5b9076b5e55835c38c08c5
Abiti0233 <gogoyon3021@gmail.com>
2023-10-24 11:13:00 +0900

    年齢が表示できるようにusers_controller.rbを編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name)
+      params.require(:user).permit(:name, :age)
     end
 end
